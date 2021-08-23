f35da5b6d620a9f157646acceaf6308f50ab2acb
Kittitad Seelawiang <Raijinz@users.noreply.github.com>
2021-08-23 18:01:48 +0700

    Revert "Order initialization"
    This reverts commit b3553141be2258cb33f894859babd150f20a3fd7.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { initFacebookSDK } from './services/initFacebookSDK'
-import { initMessengerExtSDK } from './services/initMessengerExtSDK'
 import store from './store'
 
 Vue.config.productionTip = false
 
-initMessengerExtSDK()
-  .then(initFacebookSDK())
-  .then(startApp())
-
-function startApp () {
-  new Vue({
-    router,
-    store,
-    render: h => h(App)
-  }).$mount('#app')
-}
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
