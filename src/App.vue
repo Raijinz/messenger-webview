02ef2749626665427cbcdaf6f34cb82752927f42
Kittitad Seelawiang <Raijinz@users.noreply.github.com>
2021-08-23 17:24:59 +0700

    Add Facebook JS SDK

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 
 const FACEBOOK_APP_ID = process.env.VUE_APP_FACEBOOK_APP_ID
 
@@ -30,6 +30,11 @@ export default {
       error: ''
     }
   },
+  computed: {
+    ...mapState('threadContext', {
+      psid: state => state.psid
+    })
+  },
   mounted () {
     const vm = this
 
@@ -39,23 +44,36 @@ export default {
       MessengerExtensions.getSupportedFeatures(
         function success (result) {
           vm.supportFeatures = result.supported_features
-          console.log(result)
         },
         function error (err) {
           vm.error = err
-          console.log(err)
         }
       )
 
-      console.log(FACEBOOK_APP_ID)
       MessengerExtensions.getContext(FACEBOOK_APP_ID,
         function success (threadContext) {
           vm.setThreadContext(threadContext)
-          console.log(threadContext)
         },
         function error (err) {
           vm.error = err
-          console.log(err)
+        }
+      )
+    }
+
+    window.fbAsyncInit = function () {
+      const FB = window.FB
+      FB.init({
+        appId: FACEBOOK_APP_ID,
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v11.0'
+      })
+
+      FB.api(vm.psid,
+        'GET',
+        { fields: 'id,name,first_name,last_name,profile_pic,locale,timezone,gender' },
+        function (response) {
+          vm.setUserProfile(response)
         }
       )
     }
@@ -63,6 +81,9 @@ export default {
   methods: {
     ...mapActions('threadContext', {
       setThreadContext: 'setThreadContext'
+    }),
+    ...mapActions('userProfile', {
+      setUserProfile: 'setUserProfile'
     })
   }
 }
--- src/main.js
+++ src/main.js
@@ -8,14 +8,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App),
-  created () {
-    return (function (d, s, id) {
-      var js; var fjs = d.getElementsByTagName(s)[0]
-      if (d.getElementById(id)) { return }
-      js = d.createElement(s); js.id = id
-      js.src = '//connect.facebook.net/en_US/messenger.Extensions.js'
-      fjs.parentNode.insertBefore(js, fjs)
-    }(document, 'script', 'Messenger'))
-  }
+  render: h => h(App)
 }).$mount('#app')
