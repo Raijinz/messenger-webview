91b74b329ed64e01640cc87e3459f3c83d91ec14
Kittitad Seelawiang <Raijinz@users.noreply.github.com>
2021-08-26 10:25:52 +0700

    Stop using store PSID (inconsistent timing)

--- src/App.vue
+++ src/App.vue
@@ -19,11 +19,12 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapActions } from 'vuex'
 import axios from 'axios'
 import checkTouchScreen from '@/utils/checkTouchScreen'
 import checkMobileDevice from '@/utils/checkMobileDevice'
 
+const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID
 const PAGE_ACCESS_TOKEN = process.env.VUE_APP_PAGE_ACCESS_TOKEN
 
 export default {
@@ -36,15 +37,21 @@ export default {
       isMobile: false
     }
   },
-  computed: {
-    ...mapState('threadContext', {
-      psid: state => state.psid
-    })
-  },
   created () {
     this.touchable = checkTouchScreen()
     this.isMobile = checkMobileDevice()
-    if (this.psid) this.getProfile(this.psid)
+
+    window.extAsyncInit = function () {
+      const MessengerExtensions = window.MessengerExtensions
+
+      MessengerExtensions.getContext(FACEBOOK_APP_ID,
+        function success (threadContext) {
+          this.getProfile(threadContext.psid)
+        },
+        function error (err) {
+          console.log(err)
+        })
+    }
   },
   methods: {
     ...mapActions('userProfile', {
