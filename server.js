3dde928ddf19393ac0af1530e0291bc11190af88
Kittitad Seelawiang <Raijinz@users.noreply.github.com>
2022-02-17 12:04:09 +0700

    Add helmet

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const history = require('connect-history-api-fallback')
 const path = require('path')
+const helmet = require('helmet')
 
 const app = express()
 app.use(history())
+app.use(helmet())
 app.use(express.static(path.join(__dirname, 'dist')))
 
 const port = process.env.PORT || 8000
